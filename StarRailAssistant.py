0b79cecdeb3af40d221249e6e20f8c291b96e59f
DLmaster <DLmaster_361@163.com>
2024-10-01 22:12:40 +0800

    prepare for github action
    Former-commit-id: bd860b09d17f95f5b34a8faf75fabc991116aede

--- StarRailAssistant.py
+++ StarRailAssistant.py
@@ -1,3 +1,21 @@
+#   <StarRailAssistant:An automated program that helps you complete daily tasks of StarRail.>
+#   Copyright © <2024> <Shasnow>
+
+#   This file is part of StarRailAssistant.
+
+#   StarRailAssistant is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU General Public License as published by the Free Software Foundation,
+#   either version 3 of the License, or (at your option) any later version.
+
+#   StarRailAssistant is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+#   without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#   See the GNU General Public License for more details.
+
+#   You should have received a copy of the GNU General Public License along with StarRailAssistant.
+#   If not, see <https://www.gnu.org/licenses/>.
+
+#   yukikage@qq.com
+
 """
 崩坏：星穹铁道助手
 beta v0.6
@@ -30,7 +48,7 @@ class Assistant(QThread):
         self.stop_flag = True
 
     def run(self):
-        with open("config.json", "r", encoding="utf-8") as file:
+        with open("data/config.json", "r", encoding="utf-8") as file:
             config = json.load(file)
         if not self.stop_flag:
             if config["starGame"]:
