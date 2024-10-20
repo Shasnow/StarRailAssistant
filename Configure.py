4abd12575aa89bfa25ff6a8abb505e0743acf4c4
DLmaster <DLmaster_361@163.com>
2024-10-20 22:54:47 +0800

    添加config自动更新字段能力

--- Configure.py
+++ Configure.py
@@ -28,81 +28,106 @@ import os
 
 def init() -> None:
     if not os.path.exists("data/config.json"):
-        config = {
-            "Mission": {
-                "startGame": False,
-                "trailBlazerProfile": False,
-                "redeemCode": False,
-                "assignment": False,
-                "giftOfOdyssey": False,
-                "mail": False,
-                "trailBlazePower": False,
-                "dailyTraining": False,
-                "namelessHonor": False,
-                "quitGame": False
-            },
-            "StartGame": {
-                "autoLogin": False,
-                "launcher": False,
-                "gamePath": "",
-                "pathType": "StarRail",
-                "channel": 0
-            },
-            "RedeemCode": {
-                "codeList": []
-            },
-            "Replenish": {
-                "enable": False,
-                "way": 1,
-                "runTimes": 1
-            },
-            "OrnamentExtraction": {
-                "enable": False,
-                "level": 1,
-                "runTimes": 1
-            },
-            "CalyxGolden": {
-                "enable": False,
-                "level": 1,
-                "singleTimes": 1,
-                "runTimes": 1
-            },
-            "CalyxCrimson": {
-                "enable": False,
-                "level": 1,
-                "singleTimes": 1,
-                "runTimes": 1
-            },
-            "StagnantShadow": {
-                "enable": False,
-                "level": 1,
-                "runTimes": 1
-            },
-            "CaverOfCorrosion": {
-                "enable": False,
-                "level": 1,
-                "runTimes": 1
-            },
-            "EchoOfWar": {
-                "enable": False,
-                "runTimes": 1,
-                "level": 1
-            },
-            "QuitGame": {
-                "exitSRA": False,
-                "shutdown": False,
-                "sleep": False
-            },
-            "Settings": {
-                "F1": "f1",
-                "F2": "f2",
-                "F3": "f3",
-                "F4": "f4",
-                "autoUpdate": True
-            }
-        }
-        with open("data/config.json", "w", encoding="utf-8") as json_file:
-            json.dump(config, json_file, indent=4)
+        config = {}
+        with open("data/config.json", "w") as f:
+            json.dump(config, f, indent=4)
+    config_list_1 = [
+        "Mission",
+        "StartGame",
+        "RedeemCode",
+        "Replenish",
+        "OrnamentExtraction",
+        "CalyxGolden",
+        "CalyxCrimson",
+        "StagnantShadow",
+        "CaverOfCorrosion",
+        "EchoOfWar",
+        "QuitGame",
+        "Settings",
+    ]
+    config_list_2 = {
+        "Mission": [
+            ["startGame", False],
+            ["trailBlazerProfile", False],
+            ["redeemCode", False],
+            ["assignment", False],
+            ["giftOfOdyssey", False],
+            ["mail", False],
+            ["trailBlazePower", False],
+            ["dailyTraining", False],
+            ["namelessHonor", False],
+            ["quitGame", False],
+        ],
+        "StartGame": [
+            ["autoLogin", False],
+            ["launcher", False],
+            ["gamePath", ""],
+            ["pathType", "StarRail"],
+            ["channel", 0],
+        ],
+        "RedeemCode": [
+            ["codeList", []],
+        ],
+        "Replenish": [
+            ["enable", False],
+            ["way", 1],
+            ["runTimes", 1],
+        ],
+        "OrnamentExtraction": [
+            ["enable", False],
+            ["level", 1],
+            ["runTimes", 1],
+        ],
+        "CalyxGolden": [
+            ["enable", False],
+            ["level", 1],
+            ["singleTimes", 1],
+            ["runTimes", 1],
+        ],
+        "CalyxCrimson": [
+            ["enable", False],
+            ["level", 1],
+            ["singleTimes", 1],
+            ["runTimes", 1],
+        ],
+        "StagnantShadow": [
+            ["enable", False],
+            ["level", 1],
+            ["runTimes", 1],
+        ],
+        "CaverOfCorrosion": [
+            ["enable", False],
+            ["level", 1],
+            ["runTimes", 1],
+        ],
+        "EchoOfWar": [
+            ["enable", False],
+            ["runTimes", 1],
+            ["level", 1],
+        ],
+        "QuitGame": [
+            ["exitSRA", False],
+            ["shutdown", False],
+            ["sleep", False],
+        ],
+        "Settings": [
+            ["F1", "f1"],
+            ["F2", "f2"],
+            ["F3", "f3"],
+            ["F4", "f4"],
+            ["autoUpdate", True],
+        ],
+    }
+    with open("data/config.json", "r") as f:
+        config = json.load(f)
+    for index in config_list_1:
+        if not index in config:
+            config[index] = {}
+        for config_list in config_list_2[index]:
+            if not config_list[0] in config[index]:
+                config[index][config_list[0]] = config_list[1]
+    with open("data/config.json", "w", encoding="utf-8") as f:
+        json.dump(config, f, indent=4)
 
 
 def load() -> dict:
