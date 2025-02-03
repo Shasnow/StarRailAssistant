71a37a93f44863de0cb3821f889d33f1a21e8f41
Shasnow <yukikage@qq.com>
2025-02-03 09:29:32 +0800

    update to 0.7.3 pre
    Former-commit-id: c22e1935eed12c9135c7ff78ee91822aeab71dbf

--- StarRailAssistant/utils/FuXuanDivination.py
+++ StarRailAssistant/utils/FuXuanDivination.py
@@ -1,5 +1,7 @@
+import os
+
 from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QWidget, QDialogButtonBox, QLineEdit, QComboBox, QSpinBox
+from PySide6.QtWidgets import QWidget, QDialogButtonBox, QLineEdit, QComboBox, QSpinBox, QFrame
 from PySide6.QtUiTools import QUiLoader
 from StarRailAssistant.utils.WindowsProcess import Popen
 
@@ -14,6 +16,16 @@ class FuXuanDivination(QWidget):
         self.buttonbox.button(QDialogButtonBox.StandardButton.Ok).setText("占卜")
         self.buttonbox.button(QDialogButtonBox.StandardButton.Cancel).setText("取消")
         self.buttonbox.accepted.connect(self.accept)
+        self.num_frame:QFrame=self.ui.findChild(QFrame,"NumFrame")
+        self.num_frame.setVisible(False)
+        self.divination_method_combobox:QComboBox=self.ui.findChild(QComboBox, "comboBox_2")
+        self.divination_method_combobox.currentIndexChanged.connect(self.method_change)
+
+    def method_change(self,index):
+        if index==0:
+            self.num_frame.setVisible(False)
+        else:
+            self.num_frame.setVisible(True)
 
     def accept(self):
         question_edit:QLineEdit=self.ui.findChild(QLineEdit,"lineEdit")
