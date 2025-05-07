33efe6b9b5c5a1ad73ef584e8e081007449a2796
Shasnow <yukikage@qq.com>
2025-05-07 18:24:52 +0800

    细节调整

--- SRACore/core/SRACloud.py
+++ SRACore/core/SRACloud.py
@@ -29,6 +29,9 @@ import time
 from PySide6.QtCore import QThread, Signal
 from selenium import webdriver
 from selenium.webdriver import ActionChains
+from selenium.common.exceptions import (NoSuchElementException,
+                                        StaleElementReferenceException,
+                                        TimeoutException)
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.edge.service import Service
@@ -36,21 +39,24 @@ from selenium.webdriver.edge.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
-from SRACore.core import SRAssistant
-from SRACore.utils import Encryption
-from SRACore.utils.Logger import logger, console_handler
+from SRACore.utils.SRAWebOperator import SRAWebOperator
+from SRACore.utils.Logger import logger
+import keyboard
 
 
 class CloudGame:
-    def __init__(self, passwd):
+    def __init__(self):
+        if not os.path.exists("../../data/cookies.json"):
+            logger.info("请先登录账号")
+            self.account = input("请输入账号：")
+            self.pwd = input("请输入密码：")
+            
         # 设置Edge选项
         self.edge_options = Options()
         # self.edge_options.add_argument("--headless")  # 无头模式，不显示浏览器界面
         self.edge_options.add_argument("--disable-gpu")  # 禁用GPU加速
-        self.service = Service("tools/msedgedriver.exe")  # 指定EdgeDriver的路径
+        self.service = Service("../../tools/msedgedriver.exe")  # 指定EdgeDriver的路径
         self.driver = webdriver.Edge(service=self.service, options=self.edge_options) # 初始化driver
-        self.pwd = passwd
-        self.account = Encryption.load()
 
     def __enter__(self):
         return self
@@ -70,23 +76,69 @@ class CloudGame:
 
     def _start_and_enter(self):
         driver = self.driver
+        logger.info("跳转到云游戏页面")
         url = "https://sr.mihoyo.com/cloud"
         driver.get(url)
         driver.set_window_size(1302, 855)
         driver.delete_all_cookies()
-        wait = self.login(driver)
-        time.sleep(6)
+        logger.info("登录")
+        self.login(driver)
+        keyboard.wait("esc")
+        WebDriverWait(driver, timeout=10).until(
+            expected_conditions.presence_of_element_located((By.CLASS_NAME, "left")))
         free_time = driver.find_elements(By.CLASS_NAME, "left")
         for i in free_time:
             logger.info(i.text)
         start_game_button = driver.find_element(By.CLASS_NAME, "wel-card__content--start")
         ActionChains(driver).click(start_game_button).perform()
-        wait.until(expected_conditions.presence_of_element_located((By.TAG_NAME, "video")))
+        WebDriverWait(driver, timeout=10).until(
+            expected_conditions.presence_of_element_located((By.CLASS_NAME, "van-dialog__header")))
+        van_dialog__header=driver.find_element(By.CLASS_NAME, "van-dialog__header")
+        if van_dialog__header:
+            logger.info("选择排队队列")
+            line_item=driver.find_elements(By.CLASS_NAME, "coin-prior-choose-item-main__desc__main")
+            for i in line_item:
+                if i.text=="普通队列":
+                    ActionChains(driver).click(i).perform()
+                    break
+        WebDriverWait(driver, timeout=10).until(
+            expected_conditions.presence_of_element_located((By.CLASS_NAME, "waiting-in-queue")))
+        logger.info("排队中")
+        single_row__val=driver.find_element(By.CLASS_NAME, "single-row__val")
+        wait_time_span=single_row__val.find_element(By.TAG_NAME, "span")
+        wait_time=wait_time_span.text
+        logger.info(f"预计等待时间：{wait_time} 分钟")
+        match wait_time:
+            case "<1":
+                wait_time=120
+            case "1~5":
+                wait_time=300
+            case "5~10":
+                wait_time=600
+            case "10~20":
+                wait_time=1200
+            case "20~30":
+                wait_time=1800
+            case ">30":
+                wait_time=2400
+        for i in range(1,wait_time):
+            if i % 15 == 0:
+                try:
+                    t=single_row__val.find_element(By.TAG_NAME, "span").text
+                except StaleElementReferenceException:
+                    break
+                logger.info(f"预计等待时间：{t} 分钟")
+                if t=="<1":
+                    break
+            time.sleep(1)
+        WebDriverWait(driver, timeout=300).until(
+            expected_conditions.presence_of_element_located((By.TAG_NAME, "video")))
+        logger.info("游戏启动中")
         return True
 
-    def login(self, driver):
+    def login(self, driver: WebDriver):
         wait = WebDriverWait(driver, timeout=120)
-        if os.path.exists("data/cookies.json"):
+        if os.path.exists("../../data/cookies.json"):
             driver.delete_all_cookies()
             cookies = self.load_cookies()
             for cookie in cookies:
@@ -101,7 +153,9 @@ class CloudGame:
             ActionChains(driver).send_keys_to_element(username, self.account).perform()
             ActionChains(driver).send_keys_to_element(passwd, self.pwd).perform()
             el_checkbox_inner = driver.find_element(By.CLASS_NAME, "el-checkbox__inner")
+            logger.info("勾选同意协议")
             ActionChains(driver).click(el_checkbox_inner).perform()
+            logger.info("登录")
             login_button = driver.find_element(By.TAG_NAME, "button")
             ActionChains(driver).click(login_button).perform()
             driver.switch_to.default_content()
@@ -143,12 +197,12 @@ class CloudGame:
     def save_cookies(self):
         driver = self.driver
         cookies = driver.get_cookies()
-        with open("data/cookies.json", "w") as file:
+        with open("../../data/cookies.json", "w") as file:
             json.dump(cookies, file, indent=4)
 
     @staticmethod
     def load_cookies():
-        with open("data/cookies.json", "r") as file:
+        with open("../../data/cookies.json", "r") as file:
             cookies = json.load(file)
         return cookies
 
@@ -159,7 +213,7 @@ class CloudGame:
 
     @staticmethod
     def _save_screenshot(driver: WebDriver):
-        driver.save_screenshot("res/temp.png")
+        driver.save_screenshot("../../res/temp.png")
 
     @staticmethod
     def get_window_handle(driver: WebDriver):
@@ -175,22 +229,20 @@ class SRACloud(QThread):
     def request_stop(self):
         self.assistant.stop_flag = True
 
-    def __init__(self, passwd):
+    def __init__(self):
         super().__init__()
-        self.pwd = passwd
 
     def run(self):
-        with CloudGame(self.pwd) as cloud_game:
+        with CloudGame() as cloud_game:
             cloud_game.run()
-            self.assistant = SRAssistant.Assistant('', True, cloud_game.driver)
-            self.assistant.update_signal.connect(self.sent_signal)
-            self.assistant.start()
-            self.assistant.wait()
+
+            # self.assistant = SRAssistant.Assistant('', True, cloud_game.driver)
+            # self.assistant.update_signal.connect(self.sent_signal)
+            # self.assistant.start()
+            # self.assistant.wait()
 
 
 if __name__ == "__main__":
-    logger.addHandler(console_handler)
-    pwd = input()
-    _ = SRACloud(pwd)
+    _ = SRACloud()
     _.start()
     _.wait()
