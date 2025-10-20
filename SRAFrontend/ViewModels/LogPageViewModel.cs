9b37b168652f246a75bf2e00bebd9d2397ac9b59
Shasnow <yukikage@qq.com>
2025-10-20 22:47:31 +0800

    fix: 修改程序集名称
    Former-commit-id: 99518f98b873b8dcba2e4d307534497cf068eeab [formerly 6e8690a382ca72de9953bbaa97d335b18d7b78d1]
    Former-commit-id: b0043152189fa3a26e9d6aa034f47976b5d8f698

--- SRAFrontend/ViewModels/LogPageViewModel.cs
+++ SRAFrontend/ViewModels/LogPageViewModel.cs
@@ -1,8 +1,9 @@
-﻿using SRAFrontend.Data;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using SRAFrontend.Data;
 
 namespace SRAFrontend.ViewModels;
 
-public class LogPageViewModel(): PageViewModel(PageName.Log, "\uEAFE")
+public partial class LogPageViewModel(): PageViewModel(PageName.Log, "\uEAFE")
 {
-    
+    [ObservableProperty] private string _log = " 2024-06-01 12:00:00 [信息] 应用程序启动成功。";
 }
