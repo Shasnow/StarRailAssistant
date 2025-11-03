9c6d7e5f2e93faa0766049991afbc6971cb7bcf6
Shasnow <yukikage@qq.com>
2025-11-03 17:31:53 +0800

    feat: 增强配置管理，添加加密功能和新属性
    Former-commit-id: d30c3762f19465427698975475acf7c8c7098c60 [formerly 05423133163f026e1c07f8c86fee959cc57128a1]
    Former-commit-id: 61b51443dc87d813aeb4651b2b91ee6129a72668

--- SRAFrontend/Models/Settings.cs
+++ SRAFrontend/Models/Settings.cs
@@ -9,7 +9,7 @@ public partial class Settings : ObservableObject
     /// <summary>
     ///     是否启用线程安全模式 已过时，现在使用进程而非线程来防止阻塞
     /// </summary>
-    [ObservableProperty]private bool _threadSafety = false;
+    [ObservableProperty]private bool _threadSafety;
 
     [ObservableProperty] private double _backgroundOpacity  = 0.9;
     [ObservableProperty] private int _language  = 1; // 0: English, 1: Simplified Chinese
@@ -17,14 +17,15 @@ public partial class Settings : ObservableObject
     [ObservableProperty] private double _confidenceThreshold  = 0.9; // 识图置信度阈值
     [ObservableProperty] private int _downloadChannel  = 1; // 0: Mirror, 1: GitHub
     [ObservableProperty] private int _appChannel;  // 0: stable, 1: beta
-    [ObservableProperty] private string _mirrorChyanCdk = "";
+    [ObservableProperty] [property: JsonIgnore] private string _mirrorChyanCdk = "";
+    [JsonPropertyName("MirrorChyanCdk")]public string EncryptedMirrorChyanCdk { get; set; } = "";
     [ObservableProperty] private bool _enableAutoUpdate = true; // 是否启用自动更新
     [ObservableProperty] private AvaloniaList<string> _proxies = ["https://tvv.tw/"]; // 代理列表
-    [ObservableProperty] private int _defaultPage = 0; // 启动时默认页面索引
+    [ObservableProperty] private int _defaultPage; // 启动时默认页面索引
     [JsonIgnore] public static string Version => "2.0.0"; // 应用版本号
     [ObservableProperty] private bool _allowNotifications = true; // 是否允许通知
     [ObservableProperty] private bool _allowSystemNotifications = true; // 是否允许系统通知
-    [ObservableProperty] private bool _allowEmailNotifications = false; // 是否允许邮件通知
-    [ObservableProperty] private bool _enableStartupLaunch = false; // 是否启用开机自启动
-    [ObservableProperty] private bool _enableMinimizeToTray = false; // 是否启用最小化到托盘
+    [ObservableProperty] private bool _allowEmailNotifications; // 是否允许邮件通知
+    [ObservableProperty] private bool _enableStartupLaunch; // 是否启用开机自启动
+    [ObservableProperty] private bool _enableMinimizeToTray; // 是否启用最小化到托盘
 }
