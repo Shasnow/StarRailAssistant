2d24d21c549506fd3dd3b0c5c34097017530e1ed
Shasnow <yukikage@qq.com>
2025-11-14 12:12:40 +0800

    fix: 修复打包错误
    Former-commit-id: 1cdb61ff9a13c1bd5faa1e54b23b81057bd6229f [formerly b464168c5dd5c22fdaa2d0e48aa11b60a2a4ac20]
    Former-commit-id: d742c4f1dd5688441d2a2ef3d4c75642034a6fd7

--- package.py
+++ package.py
@@ -47,7 +47,6 @@ if __name__ == "__main__":
         " --file-description='StarRailAssistant Component'"
         " --copyright='Copyright © 2024 Shasnow'"
         " --assume-yes-for-downloads --output-filename=SRA-cli"
-        " --exclude-module=tkinter"
         " --remove-output main.py"
     )
     print("Python program packaging completed !")
