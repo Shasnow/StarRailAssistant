5b277aabfbe3bdd59d6dfde6583c9a0a171738b1
Shasnow <yukikage@qq.com>
2026-02-03 16:30:33 +0800

    feat: 更新版本至2.6.0

--- SRAFrontend/Models/Settings.cs
+++ SRAFrontend/Models/Settings.cs
@@ -57,7 +57,7 @@ public partial class Settings : ObservableObject
 
     [JsonPropertyName("MirrorChyanCdk")] public string EncryptedMirrorChyanCdk { get; set; } = "";
     [JsonPropertyName("EmailAuthCode")] public string EncryptedEmailAuthCode { get; set; } = "";
-    public const string Version = "2.5.2"; // 应用版本号
+    public const string Version = "2.6.0"; // 应用版本号
 
     // 新增快捷键设置
     public string ActivityHotkey { get; set; } = "F1"; // 活动 默认 F1
