0ad3c5e2719ef0f2775b4771879f441db259493d
Shasnow <yukikage@qq.com>
2026-03-08 14:18:57 +0800

    refactor(localization): 移除__init__.py中的冗余代码并优化SRA.py中的语言设置
    将语言支持列表和当前语言设置从__init__.py移至SRA.py
    修复触发器属性设置日志信息的参数顺序
    
    
    Former-commit-id: 9132898e32ee9869e58b06047fa348bbc3923ba7 [formerly ae1d086db849f6536f2c7fa1254daf76e52ca955]
    Former-commit-id: a6c5ec7ee336454a0e069a39199b9b1717d4fe91

--- SRACore/localization/__init__.py
+++ SRACore/localization/__init__.py
@@ -1,11 +1,3 @@
 from SRACore.localization.resource import Resource
-from SRACore.util.data_persister import load_settings
-
-lang_support = [
-    'zh-cn',  # 中文
-    'en-us',  # 英文
-]
-current_lang = lang_support[load_settings().get('Language', 0)]
-Resource.set_language(current_lang)
 
 __all__ = ['Resource']
