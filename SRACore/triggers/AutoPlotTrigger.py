d9dc64e360e4471a06020a2514cb91948d6f8f2f
Shasnow <yukikage@qq.com>
2026-03-24 12:07:55 +0800

    fix: 回退对AutoPlotTrigger的更改

--- SRACore/triggers/AutoPlotTrigger.py
+++ SRACore/triggers/AutoPlotTrigger.py
@@ -1,16 +1,13 @@
-from SRACore.operators.ioperator import IOperator
 from SRACore.triggers.BaseTrigger import BaseTrigger
-from tasks.img import IMG
 
 
 class AutoPlotTrigger(BaseTrigger):
-    def __init__(self, operator: IOperator):
+    def __init__(self, operator):
         super().__init__(operator)
         self.name = '自动对话'
         self.can_skip = True  # 是否可以跳过对话, 默认可以跳过
         self.skip_plot = False  # 是否跳过对话
         self.active_window = False
-        self.dialog_options = [IMG.NUMBER_5, IMG.NUMBER_4, IMG.NUMBER_3, IMG.NUMBER_2, IMG.NUMBER_1]
 
     def run(self):
         if not self.plot_status_check():
@@ -19,10 +16,9 @@ class AutoPlotTrigger(BaseTrigger):
         self.operator.press_key("space", trace=False)
         if self.skip_plot and self.can_skip:
             self.skip_check()
-        for index, img_path in enumerate(self.dialog_options, start=1):
-            if self.operator.locate(img_path, from_x=0.63, from_y=0.375, to_x=0.67, to_y=0.72, trace=False):
-                # dialog_options is ordered 5..1, so convert index back to key value.
-                self.operator.press_key(str(6 - index), trace=False)
+        for i in range(5, 0, -1):
+            if self.operator.locate(f"resources/img/{i}.png", from_x=0.63, from_y=0.375, to_x=0.67, to_y=0.72, trace=False):
+                self.operator.press_key(str(i), trace=False)
                 break
 
     def plot_status_check(self):
@@ -32,7 +28,7 @@ class AutoPlotTrigger(BaseTrigger):
         self.operator.sleep(1)
         if not self.operator.is_window_active():
             return False
-        return self.operator.locate_any([IMG.DIALOG, IMG.M], from_x=0.067, from_y=0.033,
+        return self.operator.locate_any(["resources/img/dialog.png", "resources/img/m.png"], from_x=0.067, from_y=0.033,
                                         to_x=0.156, to_y=0.076, trace=False)[0] != -1
 
     def skip_check(self):
@@ -40,7 +36,7 @@ class AutoPlotTrigger(BaseTrigger):
         检测是否可以跳过对话
         """
         self.operator.click_point(0.8225, 0.055)  # 尝试点击跳过键
-        if self.operator.click_img(IMG.ENSURE):  # 如果出现确认跳过按钮，说明可以跳过
+        if self.operator.click_img('resources/img/ensure.png'):  # 如果出现确认跳过按钮，说明可以跳过
             self.can_skip = True
         else:
             self.can_skip = False
