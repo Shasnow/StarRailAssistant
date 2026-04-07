786f7a0dee1252d5674621df92a7c8e91ed4dd83
Shasnow <yukikage@qq.com>
2026-04-07 13:17:22 +0800

    feat: 添加遮罩窗口

---
+++ SRAFrontend/Services/OverlayService.cs
@@ -0,0 +1,38 @@
+﻿using SRAFrontend.Controls;
+
+namespace SRAFrontend.Services;
+
+public class OverlayService(IBackendService backendService)
+{
+    private OverlayWindow? _overlayWindow;
+    
+    public void ShowOverlay()
+    {
+        if (_overlayWindow == null)
+        {
+            _overlayWindow = new OverlayWindow();
+            backendService.Outputted += BackendServiceOnOutputted;
+            _overlayWindow.Closed += (_, _) =>
+            {
+                backendService.Outputted -= BackendServiceOnOutputted;
+                _overlayWindow = null;
+            };
+        }
+        _overlayWindow.Show();
+    }
+    
+    public void HideOverlay()
+    {
+        _overlayWindow?.Hide();
+    }
+
+    public void CloseOverlay()
+    {
+        _overlayWindow?.Close();
+    }
+
+    private void BackendServiceOnOutputted(string obj)
+    {
+        _overlayWindow?.AppendLog(obj);
+    }
+}
