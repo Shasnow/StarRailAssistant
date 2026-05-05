bf58f86fb08f4c9c84d6d43287b056fa203aad9c
Shasnow <yukikage@qq.com>
2026-05-05 11:38:42 +0800

    chore: 更新版本号至2.14.0-beta.1
    统一更新package.json、const.py和AppSettings.cs中的版本号，准备beta版本发布
    
    
    Former-commit-id: 72c61cfeca6263107b885e8d91502675cb5b0f45 [formerly f8782ffec13afd447d3f1c9b16678f739ad4464c]
    Former-commit-id: 88a4a6fbcb759fecc75771892be8fb5cddc3ba8f

--- SRAFrontend/Models/AppSettings.cs
+++ SRAFrontend/Models/AppSettings.cs
@@ -22,7 +22,7 @@ public class AppSettings
     [JsonPropertyName("notification")]
     public NotificationSettings Notification { get; init; } = new();
     
-    public const string Version = "2.13.0"; // 应用版本号
+    public const string Version = "2.14.0-beta.1"; // 应用版本号
 }
 
 public partial class GeneralSettings : ObservableObject
