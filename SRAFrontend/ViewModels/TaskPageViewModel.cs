7e65ecdd463244833785b3ceada51dfc6d813d8a
Shasnow <yukikage@qq.com>
2026-05-14 21:00:39 +0800

    refactor: 重构清体力任务界面
    Former-commit-id: e4006588f28adeaad0d818015b9e8ba66cc9e2c2 [formerly 7bdd708a54221554d8367cd4f5442176e4685b96]
    Former-commit-id: 8cfe206687652d3e7687556893f8cea20ee20272

--- SRAFrontend/ViewModels/TaskPageViewModel.cs
+++ SRAFrontend/ViewModels/TaskPageViewModel.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
-using Avalonia.Collections;
 using Avalonia.Controls;
 using Avalonia.Platform.Storage;
 using CommunityToolkit.Mvvm.ComponentModel;
@@ -13,37 +12,35 @@ using SRAFrontend.Controls;
 using SRAFrontend.Data;
 using SRAFrontend.Models;
 using SRAFrontend.Services;
+using SukiUI.Controls;
+using SukiUI.MessageBox;
 
 namespace SRAFrontend.ViewModels;
 
 public partial class TaskPageViewModel : PageViewModel
 {
     private readonly CacheService _cacheService;
-    private readonly ConfigService _configService;
     private readonly CommonModel _commonModel;
+    private readonly ConfigService _configService;
 
     [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(CosmicStrifeConfig), nameof(MissionAccomplishedConfig), nameof(ReceiveRewardsConfig), nameof(StartGameConfig), nameof(TrailblazePowerConfig))]
+    [NotifyPropertyChangedFor(nameof(CosmicStrifeConfig), nameof(MissionAccomplishedConfig),
+        nameof(ReceiveRewardsConfig), nameof(StartGameConfig), nameof(TrailblazePowerConfig))]
     private TasksConfig _currentConfig;
-    public CosmicStrifeConfig CosmicStrifeConfig => CurrentConfig.CosmicStrife;
-    public MissionAccomplishedConfig MissionAccomplishedConfig => CurrentConfig.MissionAccomplished;
-    public ReceiveRewardsConfig ReceiveRewardsConfig => CurrentConfig.ReceiveRewards;
-    public StartGameConfig StartGameConfig => CurrentConfig.StartGame;
-    public TrailblazePowerConfig TrailblazePowerConfig => CurrentConfig.TrailblazePower;
-    
+
+    [ObservableProperty] private bool _isTpTaskAutoDetect;
+
     [ObservableProperty] [NotifyPropertyChangedFor(nameof(EnableContextMenu))]
     private object? _selectedTaskItem;
 
-    public int CurrencyWarsStrategyIndex
-    {
-        get => CosmicStrifeConfig.CurrencyWarsStrategyIndex;
-        set
-        {
-            CosmicStrifeConfig.CurrencyWarsStrategyIndex = value;
-            OnPropertyChanged();
-            CosmicStrifeConfig.CurrencyWarsStrategy = Cache.Strategies.ElementAtOrDefault(value)?.FileName ?? "";
-        }
-    }
+    [ObservableProperty] [NotifyPropertyChangedFor(nameof(CurrentTpTaskLevels), nameof(CurrentTpTaskMaxSingleTimes))]
+    private int _selectedTpTaskIndex;
+
+    [ObservableProperty] private int _selectedTpTaskLevelIndex;
+
+    [ObservableProperty] private int _tpTaskRunTimes = 1;
+
+    [ObservableProperty] private int _tpTaskSingleTimes = 1;
 
     public TaskPageViewModel(
         CommonModel commonModel,
@@ -67,185 +64,35 @@ public partial class TaskPageViewModel : PageViewModel
 
         _cacheService.Cache.PropertyChanged += OnCachePropertyChanged;
 
-        if (Cache.Strategies.Count == 0)
+        if (Cache.Strategies.Count == 0) RefreshStrategies();
+    }
+
+    public string[] TpTaskNames => TpTaskItems.TpTaskNames;
+    public string[] CurrentTpTaskLevels => TpTaskItems.GetLevelsByIndex(SelectedTpTaskIndex);
+    public int CurrentTpTaskMaxSingleTimes => TpTaskItems.GetMaxSingleTimesByIndex(SelectedTpTaskIndex);
+    
+    public string TaskListText =>
+        TrailblazePowerConfig.TaskList.Count == 0
+            ? "暂无任务"
+            : $"{string.Join("、", TrailblazePowerConfig.TaskList.Select(x => x.Name).Take(3))} 等 {TrailblazePowerConfig.TaskList.Count} 个任务";
+    public CosmicStrifeConfig CosmicStrifeConfig => CurrentConfig.CosmicStrife;
+    public MissionAccomplishedConfig MissionAccomplishedConfig => CurrentConfig.MissionAccomplished;
+    public ReceiveRewardsConfig ReceiveRewardsConfig => CurrentConfig.ReceiveRewards;
+    public StartGameConfig StartGameConfig => CurrentConfig.StartGame;
+    public TrailblazePowerConfig TrailblazePowerConfig => CurrentConfig.TrailblazePower;
+
+    public int CurrencyWarsStrategyIndex
+    {
+        get => CosmicStrifeConfig.CurrencyWarsStrategyIndex;
+        set
         {
-            RefreshStrategies();
+            CosmicStrifeConfig.CurrencyWarsStrategyIndex = value;
+            OnPropertyChanged();
+            CosmicStrifeConfig.CurrencyWarsStrategy = Cache.Strategies.ElementAtOrDefault(value)?.FileName ?? "";
         }
     }
 
     public ControlPanelViewModel ControlPanelViewModel { get; }
-
-    public AvaloniaList<TrailblazePowerTask> Tasks => [
-            new(AddTaskItem)
-            {
-                Title = "饰品提取",
-                Id = "ornament_extraction",
-                Cost = 40,
-                Levels = new[]
-                {
-                    "---选择副本---",
-                    "鎏金追忆（朋克洛德/千星城）",
-                    "西风丛中（翁法罗斯/天国@直播间）",
-                    "月下朱殷（妖精/海隅）",
-                    "纷争不休（拾骨地/巨树）",
-                    "蠹役饥肠（露莎卡/蕉乐园）",
-                    "永恒笑剧（都蓝/劫火）",
-                    "伴你入眠（茨冈尼亚/出云显世）",
-                    "天剑如雨（格拉默/匹诺康尼）",
-                    "孽果盘生（繁星/龙骨）",
-                    "百年冻土（贝洛伯格/萨尔索图）",
-                    "温柔话语（公司/差分机）",
-                    "浴火钢心（塔利亚/翁瓦克）",
-                    "坚城不倒（太空封印站/仙舟）"
-                },
-                MaxSingleTimes = 6
-            },
-
-            new(AddTaskItem)
-            {
-                Title = "拟造花萼（金）",
-                Id = "calyx_golden",
-                Cost = 10,
-                Levels = new[]
-                {
-                    "---选择副本---",
-                    "回忆之蕾（二相乐园）",
-                    "以太之蕾（二相乐园）",
-                    "珍藏之蕾（二相乐园）",
-                    "回忆之蕾（翁法罗斯）",
-                    "以太之蕾（翁法罗斯）",
-                    "珍藏之蕾（翁法罗斯）",
-                    "回忆之蕾（匹诺康尼）",
-                    "以太之蕾（匹诺康尼）",
-                    "珍藏之蕾（匹诺康尼）",
-                    "回忆之蕾（仙舟罗浮）",
-                    "以太之蕾（仙舟罗浮）",
-                    "珍藏之蕾（仙舟罗浮）",
-                    "回忆之蕾（雅利洛VI）",
-                    "以太之蕾（雅利洛VI）",
-                    "珍藏之蕾（雅利洛VI）"
-                },
-                MaxSingleTimes = 24
-            },
-
-            new(AddTaskItem)
-            {
-                Title = "拟造花萼（赤）",
-                Id = "calyx_crimson",
-                Cost = 10,
-                Levels = new[]
-                {
-                    "---选择副本---",
-                    "月狂獠牙（毁灭）",
-                    "净世残刃（毀灭）",
-                    "神体琥珀（存护）",
-                    "琥珀的坚守（存护）",
-                    "天谴血矛（巡猎）",
-                    "逆时一击（巡猎）",
-                    "逐星之矢（巡猎）",
-                    "万象果实（丰饶）",
-                    "永恒之花（丰饶）",
-                    "精致色稿（智识）",
-                    "智识之钥（智识）",
-                    "天外乐章（同谐）",
-                    "群星乐章（同谐）",
-                    "法吉娜之心（虚无）",
-                    "焚天之魔（虚无）",
-                    "沉沦黑曜（虚无）",
-                    "阿赖耶华（记忆）",
-                    "《绒绒号》典藏版合集（欢愉）"
-                },
-                MaxSingleTimes = 24
-            },
-
-            new(AddTaskItem)
-            {
-                Title = "凝滞虚影",
-                Id = "stagnant_shadow",
-                Cost = 30,
-                Levels = new[]
-                {
-                    "---选择副本---",
-                    "侵略凝块（物理）",
-                    "星际和平工作证（物理）",
-                    "幽府通令（物理）",
-                    "铁狼碎齿（物理）",
-                    "明辉日珥（火）",
-                    "忿火之心（火）",
-                    "过热钢刀（火）",
-                    "恒温晶壳（火）",
-                    "海妖残鰭（冰）",
-                    "冷藏梦箱（冰）",
-                    "苦寒晶壳（冰）",
-                    "风雪之角（冰）",
-                    "狂雷扫弦（雷）",
-                    "兽馆之钉（雷）",
-                    "炼形者雷枝（雷）",
-                    "往日之影的雷冠（雷）",
-                    "暮辉烬蕾（风）",
-                    "一杯酪酊的时代（风）",
-                    "无人遗垢（风）",
-                    "暴风之眼（风）",
-                    "暗帷月华（量子）",
-                    "炙梦喷枪（量子）",
-                    "苍猿之钉（量子）",
-                    "虚幻铸铁（量子）",
-                    "纷争前兆（虚数）",
-                    "一曲和弦的幻景（虚数）",
-                    "镇灵敕符（虚数）",
-                    "往日之影的金饰（虚数）"
-                },
-                MaxSingleTimes = 8
-            },
-
-            new(AddTaskItem)
-            {
-                Title = "侵蚀隧洞",
-                Id = "caver_of_corrosion",
-                Cost = 40,
-                Levels = new[]
-                {
-                    "---选择副本---",
-                    "魔占之径（魔法少女/卜者）",
-                    "隐救之径（救世主/隐士）",
-                    "雳勇之径（女武神/船长）",
-                    "弦歌之径（英豪/诗人）",
-                    "迷识之径（司铎套/学者套）",
-                    "勇骑之径（铁骑套/勇烈套）",
-                    "梦潜之径（死水/钟表匠）",
-                    "幽冥之径（大公/幽囚）",
-                    "药使之径（莳者/信使）",
-                    "野焰之径（火匠/废土客）",
-                    "圣颂之径（圣骑/乐队）",
-                    "睿智之径（铁卫/量子套）",
-                    "漂泊之径（过客/快枪手）",
-                    "迅拳之径（拳皇/怪盗）",
-                    "霜风之径（冰/风套）"
-                },
-                MaxSingleTimes = 6
-            },
-
-            new(AddTaskItem)
-            {
-                Title = "历战余响",
-                Id = "echo_of_war",
-                Cost = 30,
-                Levels = new[]
-                {
-                    "---选择副本---",
-                    "铁骸的锈冢",
-                    "晨昏的回眸",
-                    "心兽的战场",
-                    "尘梦的赞礼",
-                    "蛀星的旧靥",
-                    "不死的神实",
-                    "寒潮的落幕",
-                    "毁灭的开端"
-                },
-                MaxSingleTimes = 3,
-                CanAutoDetect = false
-            }
-        ];
 
     public TopLevel? TopLevelObject { get; set; }
 
@@ -279,6 +126,7 @@ public partial class TaskPageViewModel : PageViewModel
             _commonModel.ShowErrorToast("Error", "未找到攻略文件夹，无法刷新");
             return;
         }
+
         // 遍历攻略文件夹中的json文件，反序列化成Strategy对象，并更新Cache中的Strategies列表
         var strategies = new List<Strategy>();
         foreach (var file in Directory.GetFiles(PathString.StrategiesDir))
@@ -290,6 +138,7 @@ public partial class TaskPageViewModel : PageViewModel
             strategy.FileName = Path.GetFileNameWithoutExtension(file);
             strategies.Add(strategy);
         }
+
         Cache.Strategies.Clear();
         Cache.Strategies.AddRange(strategies);
         CurrencyWarsStrategyIndex = 0;
@@ -310,9 +159,10 @@ public partial class TaskPageViewModel : PageViewModel
         if (SelectedTaskItem is TrailblazePowerTaskItem item) TrailblazePowerConfig.TaskList.Remove(item);
     }
 
-    private void AddTaskItem(TrailblazePowerTask task)
+    [RelayCommand]
+    private void AddTaskItem()
     {
-        if (task.SelectedIndex == 0)
+        if (SelectedTpTaskLevelIndex == 0)
         {
             _commonModel.ShowInfoToast("Info", "请选择副本关卡后再添加任务");
             return;
@@ -320,13 +170,40 @@ public partial class TaskPageViewModel : PageViewModel
 
         TrailblazePowerConfig.TaskList.Add(new TrailblazePowerTaskItem
         {
-            Name = task.Title,
-            Id = task.Id,
-            Level = task.SelectedIndex,
-            LevelName = (task.Levels?.Cast<string>()!).ElementAtOrDefault(task.SelectedIndex) ?? string.Empty,
-            Count = task.Count,
-            RunTimes = task.RunTimes,
-            AutoDetect = task.IsAutoDetect
+            Name = TpTaskItems.TpTaskNames[SelectedTpTaskIndex],
+            Id = TpTaskItems.TaskItems[SelectedTpTaskIndex].Id,
+            Level = SelectedTpTaskLevelIndex,
+            LevelName = CurrentTpTaskLevels.ElementAtOrDefault(SelectedTpTaskLevelIndex) ?? "",
+            Count = TpTaskSingleTimes,
+            RunTimes = TpTaskRunTimes,
+            AutoDetect = IsTpTaskAutoDetect
+        });
+    }
+
+    [RelayCommand]
+    private async Task ShowTaskListControl()
+    {
+        var taskListControl = new TpTaskListControl
+        {
+            DataContext = this
+        };
+        await SukiMessageBox.ShowDialog(new SukiMessageBoxHost
+        {
+            Content = taskListControl
+        });
+        OnPropertyChanged(nameof(TaskListText));  // 窗口关闭时更新显示文本
+    }
+
+    [RelayCommand]
+    private void ShowAddTaskControl()
+    {
+        var addTaskControl = new TpAddTaskControl
+        {
+            DataContext = this
+        };
+        SukiMessageBox.ShowDialog(new SukiMessageBoxHost
+        {
+            Content = addTaskControl
         });
     }
 }
