ccd036c475451697a8657fa3170c4a814b3c37e6
YumemizukiMizuk1 <2156471711@qq.com>
2026-06-04 21:43:33 +0800

    Add window position and size persistence (#201)
    feat: 添加窗口位置和大小记忆功能
    
    实现了issue #199，在显示设置中添加了'记忆窗口大小和位置'开关选项。
    启用后，SRA会记住窗口的位置和大小，下次启动时恢复。

--- SRAFrontend/SRAFrontend.Desktop/Views/MainWindow.axaml.cs
+++ SRAFrontend/SRAFrontend.Desktop/Views/MainWindow.axaml.cs
@@ -1,19 +1,54 @@
+using Avalonia;
+using System;
 using Avalonia.Interactivity;
 using SRAFrontend.Desktop.ViewModels;
+using SRAFrontend.Services;
 using SukiUI.Controls;
 
 namespace SRAFrontend.Desktop.Views;
 
 public partial class MainWindow : SukiWindow
 {
-    public MainWindow()
+    private readonly SettingsService _settingsService;
+
+    public MainWindow(SettingsService settingsService)
     {
+        _settingsService = settingsService;
         InitializeComponent();
+        Closed += OnClosed;
     }
 
     protected override void OnLoaded(RoutedEventArgs e)
     {
         base.OnLoaded(e);
+        var displaySettings = _settingsService.Settings.Display;
+        if (displaySettings.IsRememberWindow)
+        {
+            if (displaySettings.WindowWidth > 0 && displaySettings.WindowHeight > 0)
+            {
+                Width = displaySettings.WindowWidth;
+                Height = displaySettings.WindowHeight;
+            }
+
+            if (displaySettings.WindowPositionX != 0 || displaySettings.WindowPositionY != 0)
+            {
+                Position = new PixelPoint((int)displaySettings.WindowPositionX, (int)displaySettings.WindowPositionY);
+            }
+        }
+
         (DataContext as MainWindowViewModel)?.InitializeAsync();
+    }
+
+    private void OnClosed(object? sender, EventArgs e)
+    {
+        var displaySettings = _settingsService.Settings.Display;
+        if (displaySettings.IsRememberWindow)
+        {
+            displaySettings.WindowPositionX = Position.X;
+            displaySettings.WindowPositionY = Position.Y;
+            displaySettings.WindowWidth = Width;
+            displaySettings.WindowHeight = Height;
+            _settingsService.Save();
+        }
     }
 }
