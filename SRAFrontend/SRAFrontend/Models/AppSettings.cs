0926eeb5fffe17f44672991955ca38f69fb5be73
Shasnow <yukikage@qq.com>
2026-06-11 22:15:21 +0800

    fix: update version to 2.17.0-beta.2 in AppSettings, const.py, and package.json
    Former-commit-id: 0958ace9d18b5d8b1945123166eded9fd8f95ea8
    Former-commit-id: 5898d3c8e0ccd2652667270739569a289635d705

--- SRAFrontend/SRAFrontend/Models/AppSettings.cs
+++ SRAFrontend/SRAFrontend/Models/AppSettings.cs
@@ -6,7 +6,7 @@ namespace SRAFrontend.Models;
 
 public class AppSettings
 {
-    public const string Version = "2.17.0-beta.1"; // 应用版本号
+    public const string Version = "2.17.0-beta.2"; // 应用版本号
 
     [JsonPropertyName("general")] public GeneralSettings General { get; init; } = new();
 
