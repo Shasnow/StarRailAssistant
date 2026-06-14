b519a9459dcf8faf0edb626d5b0bda3165261f1b
Shasnow <yukikage@qq.com>
2026-06-14 14:24:33 +0800

    fix: 移除开发环境限制

--- SRAFrontend/SRAFrontend.Server/Program.cs
+++ SRAFrontend/SRAFrontend.Server/Program.cs
@@ -19,11 +19,8 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwaggerUI(options => { options.SwaggerEndpoint("/openapi/v1.json", "v1"); });
-}
+app.MapOpenApi();
+app.UseSwaggerUI(options => { options.SwaggerEndpoint("/openapi/v1.json", "v1"); });
 
 // app.UseHttpsRedirection();
 
