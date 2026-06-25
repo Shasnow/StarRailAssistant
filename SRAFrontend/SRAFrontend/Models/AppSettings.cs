5432c5efa63d7ab919498ce9aa909bf0504cdab1
Shasnow <yukikage@qq.com>
2026-06-25 21:12:51 +0800

    release: bump version to 2.18.0-beta.1
    - 更新CI触发类型为released适配新版GitHub Actions
    - 更新前端、后端、package.json的版本号为2.18.0-beta.1
    - 调整更新日志排版，将抽卡资源预测扩展的新增记录放到正确位置

--- SRAFrontend/SRAFrontend/Models/AppSettings.cs
+++ SRAFrontend/SRAFrontend/Models/AppSettings.cs
@@ -7,7 +7,7 @@ namespace SRAFrontend.Models;
 
 public class AppSettings
 {
-    public const string Version = "2.17.0"; // 应用版本号
+    public const string Version = "2.18.0-beta.1"; // 应用版本号
 
     [JsonPropertyName("general")] public GeneralSettings General { get; init; } = new();
 
