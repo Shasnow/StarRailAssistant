d8bb8bf1a8371f50c980553214c3d8be1749a073
Shasnow <yukikage@qq.com>
2026-07-25 22:19:11 +0800

    chore: bump version to 2.19.0-beta.1
    统一更新所有配置文件和代码中的版本号，从2.18.3升级到2.19.0-beta.1

--- SRAFrontend/SRAFrontend/Models/AppSettings.cs
+++ SRAFrontend/SRAFrontend/Models/AppSettings.cs
@@ -7,7 +7,7 @@ namespace SRAFrontend.Models;
 
 public class AppSettings
 {
-    public const string Version = "2.18.3"; // 应用版本号
+    public const string Version = "2.19.0-beta.1"; // 应用版本号
 
     [JsonPropertyName("general")] public GeneralSettings General { get; init; } = new();
 
